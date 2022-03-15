978148dd781b2b4ac14275102129a32735197d5c
orneirigo <ornella.irigo@gmail.com>
2022-03-15 15:13:17 -0300

    Deploy

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 import { GET_POKEMONS, GET_POKEMONS_NAME, GET_POKEMONS_ID, GET_TYPES, CREATED_POKEMON, 
         FILTER_BY_TYPES, FILTER_BY_ORIGIN, ORDER_BY_NAME, ORDER_BY_STRENGTH,} from './constans';
 
-const URLBack = "https://orne-pokemon.herokuapp.com"
+// const URLBack = "https://orne-pokemon.herokuapp.com"
 
 export function getPokemons () {
     return async (dispatch) => {
         try {
-            const allPokemons = await axios.get(`${URLBack}/pokemons`)
+            // const allPokemons = await axios.get(`${URLBack}/pokemons`)
+            const allPokemons = await axios.get('/pokemons')
             return dispatch({
                 type: GET_POKEMONS,
                 payload: allPokemons.data
@@ -35,7 +36,8 @@ export function getPokemons () {
 export function getPokemonsName (name) {
     return async (dispatch) => {
         try {
-            const pokemonName = await axios.get(`${URLBack}/pokemons/?name=${name}`)
+            // const pokemonName = await axios.get(`${URLBack}/pokemons/?name=${name}`)
+            const pokemonName = await axios.get(`/pokemons/?name=${name}`)
             return dispatch({
                 type: GET_POKEMONS_NAME,
                 payload: pokemonName.data
@@ -63,7 +65,8 @@ export function getPokemonsName (name) {
 export function getPokemonsId (id) {
     return async (dispatch) => {
         try {
-            const detail = await axios.get(`${URLBack}/pokemons/${id}`)
+            // const detail = await axios.get(`${URLBack}/pokemons/${id}`)
+            const detail = await axios.get(`/pokemons/${id}`)
             return dispatch({
                 type: GET_POKEMONS_ID,
                 payload: detail.data
@@ -91,7 +94,8 @@ export function getPokemonsId (id) {
 export function getTypes () {
     return async (dispatch) => {
         try {
-            const types = await axios.get(`${URLBack}/types/`)
+            // const types = await axios.get(`${URLBack}/types/`)
+            const types = await axios.get('/types/')
             return dispatch({
                 type: GET_TYPES,
                 payload: types.data
@@ -105,7 +109,8 @@ export function getTypes () {
 export function createdPokemon (payload) {
     return async () => {
         try {
-            const info = await axios.post(`${URLBack}/pokemons`, payload)
+            // const info = await axios.post(`${URLBack}/pokemons`, payload)
+            const info = await axios.post('/pokemons', payload)
             return ({
                 type: CREATED_POKEMON,
                 payload: info.data
