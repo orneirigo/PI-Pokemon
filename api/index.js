905d64a3e40d3eac10a23a8314cc6b50a219b161
Ornella Irigo <ornella.irigo@gmail.com>
2023-02-20 14:09:15 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@ require("dotenv").config();
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('Listening at 3001'); // eslint-disable-line no-console
+    console.log('Listening at', process.env.PORT); // eslint-disable-line no-console
   });
 });
